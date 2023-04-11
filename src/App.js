34c441b32254fbac2ab7a403c08302956584ba20
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-04-11 17:05:40 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Login from './pages/Login';
+import { Login, Wallet } from './pages/Routes';
 
 export default class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
       </Switch>
     );
   }
